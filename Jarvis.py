3bd5a2aa6e30496ab1d0fc7752999b4b3f26b62f
nikhil9935 <90110284+nikhil9935@users.noreply.github.com>
2022-02-24 02:04:34 +0530

    Create Jarvis.py

---
+++ Jarvis.py
@@ -0,0 +1,65 @@
+import pyttsx3 
+import speech_recognition as sr 
+import wikipedia 
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    speak("Hello Sir Am Your Jarvis.Please tell what should i do?")       
+
+def takeCommand():
+    #It takes voice as input from the user
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:   
+        print("Sorry I do not get it. Please Say that again please")  
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching ')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+       
+       
